76175f4da5033ed732a35a8619d17e6384750c7e
luanbrt <luanborgesteodoro@gmail.com>
2023-01-07 00:12:04 -0300

    Improvments in solution from last commit

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,8 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
-from django.forms import PasswordInput
-from crispy_forms.layout import Layout, HTML, Div, Field
+from crispy_forms.layout import Layout, Field
 
 
 class UserLoginForm(AuthenticationForm):
@@ -21,12 +20,10 @@ class UserRegistrationForm(UserCreationForm):
         super(UserRegistrationForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
 
-        self.helper.add_layout(Layout(custom_render_field(self.fields['username'].label, self.fields['username'].help_text, 'username'),
-                                      custom_render_field(self.fields['password1'].label, self.fields['password1'].help_text, 'password1'), 
-                                      custom_render_field(self.fields['password2'].label, self.fields['password2'].help_text, 'password2')))
+        self.helper.add_layout(Layout(CustomField('username'),
+                                      CustomField('password1'),
+                                      CustomField('password2')))
 
-        self.helper.form_show_errors = False
-        self.helper.form_show_labels = False
 
         # restrict user of submiting form if it's not valid
         self.helper.add_input(Submit('submit', 'Submit', css_class='btn-primary',
@@ -34,26 +31,17 @@ class UserRegistrationForm(UserCreationForm):
 
         # htmx for doing asynchronous validation on the fields
         for k, field in self.fields.items():
-            field.help_text = ''
             field.widget.attrs.update({'hx-post': reverse_lazy('accounts:register'),
                                        'autocomplete': 'off',
-                                       'hx-select-oob': f'#{k}-errors',
                                        'hx-swap': 'none',
                                        'autofocus': False, })
 
-        self.fields['username'].widget.attrs.update({'hx-select-oob': 'username-errors,password2-errors'})
-        self.fields['password1'].widget.attrs.update({'hx-select-oob': 'password1-errors,password2-errors'})
+        self.fields['username'].widget.attrs.update({'hx-select-oob': 'id_username-errors,id_password2-errors'})
+        self.fields['password1'].widget.attrs.update({'hx-select-oob': 'id_password1-errors,id_password2-errors'})
+        self.fields['password2'].widget.attrs.update({'hx-select-oob': 'id_password2-errors'})
 
+class CustomField(Field):
+    """ We create this field just to select where the errors will be swapped in the ui"""
+    template = 'accounts/snippets/custom_field.html'
 
-def custom_render_field(label, help_text, field):
-    """ Custom rendering of form field, we do that so we can access the error div for swapping """
-    error_html = """<div id="field-errors">
-                        {% for error in form.field.errors %}
-                            <span class="invalid-feedback mt-0 pb-1" ><strong>{{ error|escape }}</strong> </span>
-                        {% endfor %}
-                    </div>""".replace('field', field)
-    div = Div(HTML(f"""<label for="{ field }">{ label }</label>"""), Field(field), HTML(error_html), 
-              HTML(f"""<small class="form-text text-muted mt-0 pb-1" >{help_text}</small>"""), css_class='mb-3')
-
-    return div
 
